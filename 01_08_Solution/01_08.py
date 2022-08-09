9b040e09996160cb88d0361934e5764834cee542
Ralph Steyer <ralph.steyer@rjs.de>
2022-08-09 08:53:42 +0200

    Update 01_08.py

--- 01_08_Solution/01_08.py
+++ 01_08_Solution/01_08.py
@@ -6,7 +6,7 @@ regelzeit_s = regelzeit_m * 60 # Sekunden
 weg = 10  # km
 
 
-# Gschwindigkeit = Weg / Zeit
+# Geschwindigkeit = Weg / Zeit
 
 
 v_durchschnitt_m = weg/regelzeit_m # km/min
@@ -14,8 +14,10 @@ v_durchschnitt_m = weg/regelzeit_m # km/min
 
 v_durchschnitt_s = v_durchschnitt_m / 60 # km/s
 
-print (v_durchschnitt_m) # Geschwindigkeit in Kilometer / Minute
-print (v_durchschnitt_m * 60) # Geschwindigkeit in Kilometer / Stunde
+# Geschwindigkeit in Kilometer / Minute
+print("Geschwindigkeit in Kilometer / Minute:", v_durchschnitt_m)
+# Geschwindigkeit in Kilometer / Stunde
+print("Geschwindigkeit in Kilometer / Stunde:", v_durchschnitt_m * 60)
 
 
 verkuerzte_zeiten = (regelzeit_s - 10, regelzeit_s - 20, regelzeit_s - 30,
@@ -24,10 +26,13 @@ verkuerzte_zeiten = (regelzeit_s - 10, regelzeit_s - 20, regelzeit_s - 30,
  regelzeit_s - 100, regelzeit_s - 110, regelzeit_s - 120)
 
 for i in verkuerzte_zeiten:
-    print(weg / i * 3600)  # Erhöhte km/h
+    print("Km/h bei ", i, "Sekunden für den Weg:", weg / i * 3600)  # Erhöhte km/h
 
 max_verzoegerung = weg/vmax_s  
 
-print(max_verzoegerung)  # Minimale Zeit in Sekunden für die Strecke
+# Minimale Zeit in Sekunden für die Strecke
+print("Minimale Zeit in Sekunden für die Strecke:", max_verzoegerung)
+
+
 
 
